747a81441df4dde178317b96b3cfbcd52034823b
bsakura <13518066@std.stei.itb.ac.id>
2021-10-01 20:51:26 +0700

    fix rc4

--- modifiedrc4.py
+++ modifiedrc4.py
@@ -20,12 +20,17 @@ def ksa(N, key_1, key_2) :
     S1 = [i for i in range(N)]
     S2 = [(N-i-1) for i in range(N)]
 
-    j1 = j2 = 0
+    j1 = 0
+    j2 = 0
     for i in range(N) :
         j1 = (j1 + S1[i] + ord(key_1[i % len(key_1)])) % N
         j2 = (j2 + S2[i] + ord(key_2[i % len(key_2)])) % N
-        S1[i], S1[j1] = S1[j1], S1[i]
-        S2[i], S2[j2] = S2[j2], S2[i]
+        temp  = S1[i]
+        S1[i] = S1[j1]
+        S1[j1] = temp
+        temp  = S2[i]
+        S2[i] = S2[j1]
+        S2[j1] = temp
 
     XOR = ''
     idx1 = idx2 = 0
@@ -35,26 +40,39 @@ def ksa(N, key_1, key_2) :
         idx2 = (idx2 + 1) % len(key_2)
         XOR += chr(ord(key_1[idx1 % len(key_1)]) ^ ord(key_2[idx2 % len(key_2)]))
 
-    j1 = j2 = 0
+    j1 = 0
+    j2 = 0
     for i in range(N//2) :
         j1 = ((j1 + S1[N//2-i-1]) ^ (ord(key_1[(N//2-i-1) % len(key_1)]) + ord(XOR[(N//2-i-1) % len(XOR)]))) % N
         j2 = ((j2 + S2[N//2-i-1]) ^ (ord(key_2[(N//2-i-1) % len(key_2)]) + ord(XOR[(N//2-i-1) % len(XOR)]))) % N
-        S1[(N//2-i-1)], S1[j1] = S1[j1], S1[(N//2-i-1)]
-        S2[(N//2-i-1)], S2[j2] = S2[j2], S2[(N//2-i-1)]
+        temp  = S1[(N//2-i-1)]
+        S1[(N//2-i-1)] = S1[j1]
+        S1[j1] = temp
+        temp  = S2[(N//2-i-1)]
+        S2[(N//2-i-1)] = S2[j1]
+        S2[j1] = temp
 
     for i in range(N//2, N) :
         j1 = ((j1 + S1[i]) ^ (ord(key_1[i % len(key_1)]) + ord(XOR[i % len(XOR)]))) % N
         j2 = ((j2 + S2[i]) ^ (ord(key_2[i % len(key_2)]) + ord(XOR[i % len(XOR)]))) % N
-        S1[i], S1[j1] = S1[j1], S1[i]
-        S2[i], S2[j2] = S2[j2], S2[i]
+        temp  = S1[i]
+        S1[i] = S1[j1]
+        S1[j1] = temp
+        temp  = S2[i]
+        S2[i] = S2[j1]
+        S2[j1] = temp
 
     j1 = j2 = 0
     for count in range(N) :
         i = count//2 if count % 2 == 0 else N-(count+1)//2
         j1 = (j1 + S1[i] + ord(key_1[i % len(key_1)])) % N
         j2 = (j2 + S2[i] + ord(key_2[i % len(key_2)])) % N
-        S1[i], S1[j1] = S1[j1], S1[i]
-        S2[i], S2[j2] = S2[j2], S2[i]
+        temp  = S1[i]
+        S1[i] = S1[j1]
+        S1[j1] = temp
+        temp  = S2[i]
+        S2[i] = S2[j1]
+        S2[j1] = temp
 
     return S1, S2
 
@@ -64,13 +82,18 @@ def prga(S1, S2, N, plaintext) :
 
     keystream = ''
     i = 0
-    j1 = j2 = 0
+    j1 = 0
+    j2 = 0
     for idx in range(len(plaintext)) :
         i = (i + 1) % N
         j1 = (j1 + S1[i]) % N
         j2 = (j2 + S2[i]) % N
-        S1[i], S1[j1] = S1[j1], S1[i]
-        S2[i], S2[j2] = S2[j2], S2[i]
+        temp  = S1[i]
+        S1[i] = S1[j1]
+        S1[j1] = temp
+        temp  = S2[i]
+        S2[i] = S2[j1]
+        S2[j1] = temp
         t = ((S1[i] + S1[j1]) + (S2[i] + S2[j2])) % N
         keystream += chr(S1[t] ^ S2[t])
 
@@ -150,3 +173,9 @@ a= encrypt("aku mau makan kokumi panggang", "helo","hobo")
 print(a)
 b = decrypt(a, "helo", "hobo")
 print(b)
+c= open('hello.txt', 'r') 
+inp_file = bytearray(c.read(), encoding='utf8')
+d= encrypt_binary(inp_file,  "helo", "hobo")
+print(d)
+f=decrypt_binary(d,  "helo", "hobo")
+print(f)
